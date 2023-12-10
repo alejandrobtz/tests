f16e6fde1946b7182106ccd927ba249b09969848
Alejandro <mabtzr@gmail.com>
2023-12-10 16:11:01 -0500

    testing a parameter value in an Observable

--- src/app/advanced/router-doctor/router-doctor.component.spec.ts
+++ src/app/advanced/router-doctor/router-doctor.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterDoctorComponent } from './router-doctor.component';
 import { ActivatedRoute, Router } from '@angular/router';
-import { EMPTY, Observable } from 'rxjs';
+import { EMPTY, Observable, Subject } from 'rxjs';
 
 
 class FakeRouter {
@@ -11,7 +11,15 @@ class FakeRouter {
 }
 
 class FakeActivatedRoute {
-	params: Observable<any> = EMPTY;
+	//params: Observable<any> = EMPTY;
+	private subject = new Subject();
+	push( value: any ) {
+		this.subject.next( value );
+	}
+
+	get params() {
+		return this.subject.asObservable();
+	}
 }
 
 describe('RouterDoctorComponent', () => {
@@ -30,13 +38,25 @@ describe('RouterDoctorComponent', () => {
 
 
 	it('Must redirect to doctor when is saved', () => {
-		const router = TestBed.get(Router);
+		const router = TestBed.inject(Router);
 		const spy = spyOn(router, 'navigate');
 
 		component.saveDoctor();
 
 		expect( spy ).toHaveBeenCalledWith(['doctor', '123']);
 
-	})
+	});
+
+	it( 'should add the id === new', () => {
+		component = fixture.componentInstance;
+
+		const activatedRoute = TestBed.inject(ActivatedRoute) as unknown as FakeActivatedRoute; //typed as any for avoiding compilation errors.
+
+		activatedRoute.push({ id:'new' });
+
+		expect( component.id ).toBe('new');
+
+
+	});
 
 });
